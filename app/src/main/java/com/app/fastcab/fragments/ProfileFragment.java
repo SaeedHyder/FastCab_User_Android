3be09ca675c88f96b046c08bf02a29ac63fbcdd0
saeedhyder <saeedhyder@axact.com>
2017-06-23 21:14:26 +0500

    Signed-off-by: saeedhyder <saeedhyder@axact.com>

---
+++ app/src/main/java/com/app/fastcab/fragments/ProfileFragment.java
@@ -0,0 +1,60 @@
+package com.app.fastcab.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.app.fastcab.R;
+import com.app.fastcab.fragments.abstracts.BaseFragment;
+import com.app.fastcab.ui.views.TitleBar;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by saeedhyder on 6/23/2017.
+ */
+
+public class ProfileFragment extends BaseFragment implements View.OnClickListener {
+
+    public static EntryCodeFragment newInstance() {
+        return new EntryCodeFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        setListners();
+    }
+
+    private void setListners() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_submit:
+                getDockActivity().replaceDockableFragment(HomeFragment.newInstance(),HomeFragment.class.getSimpleName());
+                break;
+        }
+    }
+
+    @Override
+    public void setTitleBar(TitleBar titleBar) {
+        super.setTitleBar(titleBar);
+        titleBar.hideButtons();
+        titleBar.showBackButton();
+        titleBar.setSubHeading("Profile");
+    }
+}
