366cb8608e2e1d9f07e454587fd716208f1bea22
saeedhyder <saeedhyder@axact.com>
2017-07-12 21:05:18 +0500

    Signed-off-by: saeedhyder <saeedhyder@axact.com>

--- app/src/main/java/com/app/fastcab/fragments/ChangePasswordFragment.java
+++ app/src/main/java/com/app/fastcab/fragments/ChangePasswordFragment.java
@@ -2,12 +2,15 @@ package com.app.fastcab.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ToggleButton;
 
 import com.app.fastcab.R;
 import com.app.fastcab.fragments.abstracts.BaseFragment;
@@ -17,8 +20,6 @@ import com.scottyab.showhidepasswordedittext.ShowHidePasswordEditText;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-
-import static com.app.fastcab.R.id.edtPassword;
 
 /**
  * Created by saeedhyder on 6/22/2017.
@@ -37,17 +38,22 @@ public class ChangePasswordFragment extends BaseFragment implements View.OnClick
     @BindView(R.id.iv_NewPasswordIcon)
     ImageView ivNewPasswordIcon;
     @BindView(R.id.editNewPassword)
-    ShowHidePasswordEditText editNewPassword;
+    AnyEditTextView editNewPassword;
     @BindView(R.id.ll_NewPassword)
     LinearLayout llNewPassword;
     @BindView(R.id.iv_ConfirmPasswordIcon)
     ImageView ivConfirmPasswordIcon;
     @BindView(R.id.editConfirmPassword)
-    ShowHidePasswordEditText editConfirmPassword;
+    AnyEditTextView editConfirmPassword;
     @BindView(R.id.ll_ConfirmPassword)
     LinearLayout llConfirmPassword;
     @BindView(R.id.UpdateButton)
     Button UpdateButton;
+    @BindView(R.id.togglePassword)
+    ToggleButton togglePassword;
+    @BindView(R.id.togglePassword1)
+    ToggleButton togglePassword1;
+
 
     public static ChangePasswordFragment newInstance() {
         return new ChangePasswordFragment();
@@ -70,40 +76,72 @@ public class ChangePasswordFragment extends BaseFragment implements View.OnClick
 
     private void setListners() {
         UpdateButton.setOnClickListener(this);
+        togglePassword.setOnClickListener(this);
+        togglePassword1.setOnClickListener(this);
 
     }
 
-    private boolean isvalidate()
-    {
+    private boolean isvalidate() {
         if (edtcurrentPassword.getText() == null || (edtcurrentPassword.getText().toString().isEmpty())) {
             edtcurrentPassword.setError(getString(R.string.enter_password));
             return false;
+        } else if (editNewPassword.getText() == null || (editNewPassword.getText().toString().isEmpty())) {
+            editNewPassword.setError(getString(R.string.enter_password));
+            return false;
+        } else if (editNewPassword.getText().toString().length() < 6) {
+            editNewPassword.setError(getString(R.string.passwordLength));
+            return false;
+        } else if (editConfirmPassword.getText() == null || (editConfirmPassword.getText().toString().isEmpty()) || editConfirmPassword.getText().toString().length() < 6) {
+            editConfirmPassword.setError(getString(R.string.enter_password));
+            return false;
+        } else if (!editConfirmPassword.getText().toString().equals(editNewPassword.getText().toString())) {
+            editConfirmPassword.setError("confirm password does not match");
+            return false;
+        } else {
+            return true;
         }
-        else if (editNewPassword.getText() == null || (editNewPassword.getText().toString().isEmpty())) {
-        editNewPassword.setError(getString(R.string.enter_password));
-        return false;
-    } else if (editNewPassword.getText().toString().length() < 6) {
-        editNewPassword.setError(getString(R.string.passwordLength));
-        return false;
-    } else if (editConfirmPassword.getText() == null || (editConfirmPassword.getText().toString().isEmpty()) || editConfirmPassword.getText().toString().length() < 6) {
-        editConfirmPassword.setError(getString(R.string.enter_password));
-        return false;
-    } else if (!editConfirmPassword.getText().toString().equals(editNewPassword.getText().toString())) {
-        editConfirmPassword.setError("confirm password does not match");
-        return false;
-    } else {
-        return true;
-    }
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.UpdateButton:
-               if(isvalidate())
-               {
-                   getDockActivity().replaceDockableFragment(SettingFragment.newInstance(), SettingFragment.class.getSimpleName());
-               }
+                if (isvalidate()) {
+                    getDockActivity().replaceDockableFragment(SettingFragment.newInstance(), SettingFragment.class.getSimpleName());
+                }
+                break;
+
+            case R.id.togglePassword:
+
+                boolean isCheck = togglePassword.isChecked();
+
+                if (isCheck) {
+                    editNewPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    editNewPassword.setSelection(editNewPassword.getText().length());
+                } else {
+                    editNewPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    editNewPassword.setSelection(editNewPassword.getText().length());
+                }
+
+
+                break;
+
+            case R.id.togglePassword1:
+
+                boolean isCheck1 = togglePassword1.isChecked();
+
+                if (isCheck1) {
+                    editConfirmPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    editConfirmPassword.setSelection(editConfirmPassword.getText().length());
+                } else {
+                    editConfirmPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    editConfirmPassword.setSelection(editConfirmPassword.getText().length());
+                }
+
+
+                break;
+
+
         }
     }
 
